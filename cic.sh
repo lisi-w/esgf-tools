f614636067b378a48b396525c2e212d303d85bf9
lisi-w <55603732+lisi-w@users.noreply.github.com>
2020-12-11 10:33:02 -0800

    Update cic.sh

--- cic.sh
+++ cic.sh
@@ -4,4 +4,4 @@ cd /export/witham3/etools
 export PATH=~/anaconda2/bin:$PATH
 . ~/.bashrc
 conda activate cic
-python3 cic.py /p/user_pub/inconsistencies/ /export/witham3/cmor/
+python3 cic.py /p/user_pub/publish-queue/inconsistencies/ /export/witham3/cmor/
